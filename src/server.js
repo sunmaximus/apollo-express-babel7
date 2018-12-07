ec13ef5e73d76fd03b1936b8f940efa6bc7ae909
sunmaximus <son_nguyen@student.uml.edu>
2018-12-07 03:57:56 -0500

    add an example of mongoose

--- src/server.js
+++ src/server.js
@@ -1,33 +1,50 @@
 import express from 'express';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
-import { ApolloServer, gql } from 'apollo-server-express';
+import graphqlHTTP from 'express-graphql';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import schema from './graphql';
 
 dotenv.config();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/blog';
 
-// Construct a schema, using GraphQL schema language
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
-
-// Provide resolver functions for your schema fields
-const resolvers = {
-  Query: {
-    hello: () => 'Hello world!',
-  },
+// Latest version need to be fix on ObjectID
+// https://github.com/graphql/graphql-js/issues/1518
+const { ObjectId } = mongoose.Types;
+ObjectId.prototype.valueOf = function() {
+  return this.toString();
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+// Monoose DeprecationWarning collection.ensureIndex and useNewUrlParser [duplicate]
+// https://stackoverflow.com/questions/53394007/monoose-deprecationwarning-collection-ensureindex-and-usenewurlparser
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useCreateIndex', true);
+
+mongoose.connect(MONGO_URL);
+mongoose.connection.once('open', () => {
+  console.log('conneted to database');
+});
+
 
 const app = express();
 app.use(express.json());
 app.use(helmet());
+// allow cross-origin requests. This should be disable on production?
+app.use(cors());
 
-server.applyMiddleware({ app });
+app.get('/', (req, res) => {
+  res.send('Hello World..');
+});
 
-app.listen({ port }, () =>
-  console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`),
-);
+// GraphQL API
+app.use('/graphql', graphqlHTTP(() => ({
+  schema,
+  graphiql: true,
+  pretty: true,
+})));
+
+app.listen(port, () => {
+  console.log(`🚀 Server ready at http://localhost:${port}/graphql\n`);
+});
