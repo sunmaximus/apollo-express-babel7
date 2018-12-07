e28b055fd1fbde94a2af55a2b67712b3d92370db
sunmaximus <son_nguyen@student.uml.edu>
2018-12-07 02:29:07 -0500

    simple apollo exmaple

---
+++ src/server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+import { ApolloServer, gql } from 'apollo-server-express';
+
+dotenv.config();
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const app = express();
+app.use(express.json());
+app.use(helmet());
+
+server.applyMiddleware({ app });
+
+const port = process.env.PORT || 4000;
+
+app.listen({ port }, () =>
+  console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`),
+);
